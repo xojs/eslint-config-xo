9ce12516810657638be7245e6b2c5cbdd0d4ffb4
Sindre Sorhus <sindresorhus@gmail.com>
2015-08-29 15:50:32 +0700

    bump `ava`

--- test/test.js
+++ test/test.js
@@ -18,13 +18,13 @@ test('main', function (t) {
 	clearRequire.all();
 	var conf = require('../');
 
-	t.assert(isPlainObj(conf));
-	t.assert(isPlainObj(conf.env));
-	t.assert(isPlainObj(conf.rules));
+	t.true(isPlainObj(conf));
+	t.true(isPlainObj(conf.env));
+	t.true(isPlainObj(conf.rules));
 
 	var errors = runEslint('\'use strict\';\nconsole.log("unicorn")\n', conf);
-	t.assert(errors[0].ruleId === 'quotes');
-	t.assert(errors[1].ruleId === 'semi');
+	t.is(errors[0].ruleId, 'quotes');
+	t.is(errors[1].ruleId, 'semi');
 
 	t.end();
 });
@@ -33,12 +33,12 @@ test('esnext', function (t) {
 	clearRequire.all();
 	var conf = require('../esnext');
 
-	t.assert(isPlainObj(conf));
-	t.assert(isPlainObj(conf.env));
-	t.assert(isPlainObj(conf.rules));
+	t.true(isPlainObj(conf));
+	t.true(isPlainObj(conf.env));
+	t.true(isPlainObj(conf.rules));
 
 	var errors = runEslint('class Foo {}\n', conf);
-	t.assert(errors[0].ruleId === 'no-unused-vars');
+	t.is(errors[0].ruleId, 'no-unused-vars');
 
 	t.end();
 });
@@ -47,12 +47,12 @@ test('browser', function (t) {
 	clearRequire.all();
 	var conf = require('../browser');
 
-	t.assert(isPlainObj(conf));
-	t.assert(isPlainObj(conf.env));
-	t.assert(isPlainObj(conf.rules));
+	t.true(isPlainObj(conf));
+	t.true(isPlainObj(conf.env));
+	t.true(isPlainObj(conf.rules));
 
 	var errors = runEslint('\'use strict\';\nprocess.exit();\n', conf);
-	t.assert(errors[0].ruleId === 'no-undef');
+	t.is(errors[0].ruleId, 'no-undef');
 
 	t.end();
 });
