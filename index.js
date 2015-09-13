9d7d8d6975a8c4f9cdcc771b98b8c77f1621d7cc
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-13 13:22:33 +0700

    remove `{keywords: true}` in `quote-props` rule
    The docs were misleading. There are no reserved keywords in object literals in ES5+, only ES3.

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ module.exports = {
 		'operator-assignment': [2, 'always'],
 		'operator-linebreak': [2, 'after'],
 		'padded-blocks': [2, 'never'],
-		'quote-props': [2, 'consistent-as-needed', {keywords: true}],
+		'quote-props': [2, 'consistent-as-needed'],
 		'quotes': [2, 'single'],
 		'semi-spacing': [2, {before: false, after: true}],
 		'semi': [2, 'always'],
