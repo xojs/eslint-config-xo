36ec58529a5e16cf2efbb820a16877ba1ba0aab2
Sindre Sorhus <sindresorhus@gmail.com>
2016-02-14 13:12:27 +0700

    add some new eslint-plugin-babel rules

--- esnext.js
+++ esnext.js
@@ -17,14 +17,19 @@ module.exports = {
 		'prefer-spread': 2,
 
 		// disable builtin rules that are incompatible with Babel plugin ones
-		'object-shorthand': 0,
 		'generator-star-spacing': 0,
-		'arrow-parens': 0,
+		'new-cap': 0,
+		'array-bracket-spacing': 0,
 		'object-curly-spacing': 0,
+		'object-shorthand': 0,
+		'arrow-parens': 0,
 
-		'babel/object-shorthand': [2, 'always'],
 		'babel/generator-star-spacing': [2, 'both'],
+		'babel/new-cap': [2, {newIsCap: true, capIsNew: true}],
+		'babel/array-bracket-spacing': [2, 'never'],
+		'babel/object-curly-spacing': [2, 'never'],
+		'babel/object-shorthand': [2, 'always'],
 		'babel/arrow-parens': [2, 'as-needed'],
-		'babel/object-curly-spacing': [2, 'never']
+		'babel/no-await-in-loop': 2
 	}
 };
