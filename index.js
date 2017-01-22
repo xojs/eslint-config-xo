8c4100beb8f4c66e4a2a1ededda98c2b8def68ef
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-23 03:21:00 +0700

    Add `no-multi-assign` rule
    http://eslint.org/docs/rules/no-multi-assign

--- index.js
+++ index.js
@@ -188,6 +188,7 @@ module.exports = {
 		'no-lonely-if': 'error',
 		'no-mixed-operators': 'error',
 		'no-mixed-spaces-and-tabs': 'error',
+		'no-multi-assign': 'error',
 		'no-multiple-empty-lines': ['error', {
 			max: 1
 		}],
