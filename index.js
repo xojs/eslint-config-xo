cba2ede40fe4e5ea5d11329d7a01f88956de6a4b
Federico Brigante <github@bfred.it>
2017-09-06 00:46:30 +0700

    Drop linebreak-style
    git autocrlf will cause errors by default on all windows machines with this
    
    The same behavior also renders the rule anti-git defaults

--- index.js
+++ index.js
@@ -182,7 +182,6 @@ module.exports = {
 			afterColon: true
 		}],
 		'keyword-spacing': 'error',
-		'linebreak-style': ['error', 'unix'],
 		'max-depth': 'warn',
 		'max-nested-callbacks': ['warn', 4],
 		'max-params': ['warn', {
