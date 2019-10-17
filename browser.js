edf6cda955753516a8145ddc1086f06763bdbc7c
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-17 15:04:21 +0800

    Disable some confusing browser globals

--- browser.js
+++ browser.js
@@ -1,10 +1,14 @@
 'use strict';
 const path = require('path');
+const restrictedGlobals = require('confusing-browser-globals');
 
 module.exports = {
 	extends: path.join(__dirname, 'index.js'),
 	env: {
 		node: false,
 		browser: true
+	},
+	rules: {
+		'no-restricted-globals': ['error'].concat(restrictedGlobals)
 	}
 };
