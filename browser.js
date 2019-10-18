bfa20fd0e9d7403938835d1c2d075ba6725e2b73
Yang Mingshan <y.mingshan3@gmail.com>
2019-10-18 11:51:59 +0800

    Disable some confusing browser globals (#58)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- browser.js
+++ browser.js
@@ -1,10 +1,17 @@
 'use strict';
 const path = require('path');
+const confusingBrowserGlobals = require('confusing-browser-globals');
 
 module.exports = {
 	extends: path.join(__dirname, 'index.js'),
 	env: {
 		node: false,
 		browser: true
+	},
+	rules: {
+		'no-restricted-globals': [
+			'error',
+			...confusingBrowserGlobals
+		]
 	}
 };
