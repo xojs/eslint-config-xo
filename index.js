0cd50823e27afcfbc66af9632a6098a909c85a8a
Sindre Sorhus <sindresorhus@gmail.com>
2021-02-13 17:23:24 +0700

    Add `enforceForJSX` option to `no-unused-expressions`

--- index.js
+++ index.js
@@ -164,7 +164,12 @@ module.exports = {
 		'no-sequences': 'error',
 		'no-throw-literal': 'error',
 		'no-unmodified-loop-condition': 'error',
-		'no-unused-expressions': 'error',
+		'no-unused-expressions': [
+			'error',
+			{
+				enforceForJSX: true
+			}
+		],
 		'no-unused-labels': 'error',
 		'no-useless-call': 'error',
 		'no-useless-concat': 'error',
