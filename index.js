7d89a3d4eab852c2c2336dadd38286645827b69f
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-24 14:01:37 +0700

    Ban `Buffer` in favor of `Uint8Array`

--- index.js
+++ index.js
@@ -222,6 +222,10 @@ module.exports = {
 		'no-restricted-globals': [
 			'error',
 			'event',
+			{
+				name: 'Buffer',
+				message: 'Use Uint8Array instead. See: https://sindresorhus.com/blog/goodbye-nodejs-buffer',
+			},
 		],
 		'no-shadow-restricted-names': 'error',
 		'no-undef-init': 'error',
@@ -252,6 +256,14 @@ module.exports = {
 			'sys',
 			'querystring',
 			'colors',
+			{
+				name: 'buffer',
+				message: 'Use Uint8Array instead. See: https://sindresorhus.com/blog/goodbye-nodejs-buffer',
+			},
+			{
+				name: 'node:buffer',
+				message: 'Use Uint8Array instead. See: https://sindresorhus.com/blog/goodbye-nodejs-buffer',
+			},
 		],
 		'array-bracket-newline': [
 			'error',
